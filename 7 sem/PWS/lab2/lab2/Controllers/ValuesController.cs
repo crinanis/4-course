5432639ccd40868d6da690c2424688db07faaff1
ksesha <72476438+crinanis@users.noreply.github.com>
2024-02-07 12:05:41 +0300

    Add files via upload

---
+++ 7 sem/PWS/lab2/lab2/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System.Web.Http;
+
+namespace lab2.Controllers
+{
+    public class ValuesController : ApiController
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return Models.Data.Get();
+        }
+
+        [HttpPost]
+        public string Post([FromBody] string result)
+        {
+            int number;
+            if (int.TryParse(result, out number))
+            {
+                Models.Data.Post(number);
+
+                return Get();
+            }
+
+            return Get();
+        }
+
+        [HttpPut]
+        public string Put([FromBody] string add)
+        {
+            int number;
+            if (int.TryParse(add, out number))
+            {
+                Models.Data.Put(number);
+            }
+
+            return Get();
+        }
+
+
+        [HttpDelete]
+        public string Delete()
+        {
+            Models.Data.Delete();
+            return Get();
+        }
+    }
+}
---
+++ 7 sem/PWS/lab2/lab2/Models/Data.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lab2.Models
+{
+    public class Data
+    {
+        public static int result = 0;
+        public static Stack<int> stack = new Stack<int>();
+
+        public static string Get()
+        {
+            int _result = result;
+            if (stack != null)
+            {
+                if (stack.Count != 0)
+                {
+                    _result += stack.Peek();
+                }
+            }
+
+            string json = JsonConvert.SerializeObject(new { RESULT = _result });
+            return json;
+        }
+
+        public static void Post(int _result)
+        {
+            result = _result;
+        }
+
+        public static void Put(int add)
+        {
+            stack.Push(add);
+        }
+
+        public static void Delete()
+        {
+            if (stack.Count() != 0)
+            {
+                stack.Pop();
+            }
+        }
+
+        public static int StackState()
+        {
+            return stack.Count();
+        }
+    }
+}
